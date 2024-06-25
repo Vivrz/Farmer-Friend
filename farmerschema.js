6084d12c15169fc3521753c0ce352ec9b65ef815
vivrz <viveksingh@09102003@gmail.com>
2024-06-25 15:16:48 +0530

    farmerschema.js file added

---
+++ farmerschema.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+const farmerschema = new mongoose.Schema({
+    name: {
+        type: String,
+        require: true,
+        trim: true
+    },
+    contact: {
+        type: Number,
+        require: true,
+    },
+    Email: {
+        type: String,
+        trim: true
+    },
+    password: {
+        type: String,
+        trim: true,
+        require: true
+    },
+    District: {
+        type: String,
+        trim: true,
+        require: true
+    }
+
+})
+
+const Farmer = mongoose.model("Farmers", farmerschema);
+
+module.exports = Farmer;
